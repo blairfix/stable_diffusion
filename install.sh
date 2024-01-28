9f28af05b877b67be5b6e16e00a382d3677a0773
blairfix <blairfix@fastmail.com>
2024-01-28 08:26:58 -0700

    typo

--- install.sh
+++ install.sh
@@ -14,9 +14,8 @@ cd stablediff-models/
 wget https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt
 
 
-
-
 # build
+cd ..
 docker-compose build stablediff-cpu
 
 # run
